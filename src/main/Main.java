f8fe4b7cbf0e67b57c453240d98f8b83886e96e5
Gregory Golanko <grzegorz.golanko@gmail.com>
2017-07-23 14:00:25 +0200

    Algorytmy uruchamiane wielowątkowo

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,26 +1,15 @@
 package main;
 
+import java.util.Arrays;
+
 import algorithms.SortType;
 
 public class Main {
 
 	public static void main(String[] args) {
-		MainSort mainSort = new MainSort();
-		Integer [] tabBubble = randomValues(30000, 100);
-		Integer [] tabHeap = tabBubble.clone();
-		Integer [] tabInsertion = tabBubble.clone();
-		Integer [] tabQuick = tabBubble.clone();
-		Integer [] tabSelection = tabBubble.clone();
-		
-		System.out.println("Bubble Czas sortowania: " + mainSort.sort(tabBubble) / 1000.0 + "s");
-		mainSort.setSortType(SortType.HEAP);
-		System.out.println("Heap Czas sortowania: " +  mainSort.sort(tabHeap) / 1000.0 + "s");
-		mainSort.setSortType(SortType.INSERTION);
-		System.out.println("Insertion Czas sortowania: " +  mainSort.sort(tabInsertion) / 1000.0 + "s");
-		mainSort.setSortType(SortType.QUICK);
-		System.out.println("Quick Czas sortowania: " +  mainSort.sort(tabQuick) / 1000.0 + "s");
-		mainSort.setSortType(SortType.SELECTION);
-		System.out.println("Selection Czas sortowania: " +  mainSort.sort(tabSelection) / 1000.0 + "s");
+		Arrays.asList(SortType.values()).forEach(type -> {
+			startSort(type);
+		});
 	}
 	
 	
@@ -32,10 +21,17 @@ public class Main {
 		return values;
 	}
 	
-	private static void printValues(Integer[] values) {
-		System.out.print("Values: ");
-		for (Integer i : values) {
-			System.out.print(i + " ");
-		}
+	private static void startSort(SortType type) {
+		MainSort mainSort = new MainSort();
+		Integer [] values = randomValues(30000, 100);
+		mainSort.setSortType(type);
+		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println(type.name() + " czas sortowania: " + mainSort.sort(values) / 1000.0 + "s");		
+			}
+		}).start();
 	}
 }
